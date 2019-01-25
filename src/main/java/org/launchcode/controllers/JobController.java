29efe9ee63f9f6b8318ab163080f69cab27a78fc
Sarah Dixon <dixonspectives@gmail.com>
2019-01-24 19:28:34 -0600

    Final commit!

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -52,7 +52,7 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "redirect:/job?id={jobId}";
+        return "redirect:/job?id=" + jobId;
 
     }
 }
